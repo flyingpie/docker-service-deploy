94b7c38ecd736e89867abaf01421d361e22159df
Marco van den Oever <marco.vandenoever@datarotonde.com>
2021-02-07 17:28:06 +0100

    Workspace

---
+++ test-service/10-Host/SampleAPI/Endpoints/Utils/ReadFileEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Flyingpie.DockerServiceDeploy.SampleAPI.Endpoints.Utils
+{
+	[Route("utils")]
+	public class ReadFileEndpoint : BaseAsyncEndpoint
+	{
+		[HttpGet("read-file")]
+		public async Task<string> ReadFileAsync(string path)
+		{
+			var contents = await System.IO.File.ReadAllTextAsync(path);
+
+			return contents;
+		}
+	}
+}
